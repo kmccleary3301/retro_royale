0b9d6d7dc5c037476cba34731c733dcac832dbdb
kmccleary3301 <kmccl24@lsu.edu>
2022-10-02 14:31:12 -0500

    modified:   js/sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,7 @@
 var current_state = new fruitGame();  //Games are stored as functions in the style of a class. This is how we'll organize multiple nested games.
 
 
-/*
+/* :)
 P5 has several default functions.
 These include, but are not limited to:
 preload(), setup(), draw(), keyPressed(), keyReleased, mousePressed(), and mouseReleased().
