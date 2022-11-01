540ca0d39b97ce24a7c4508544085bb81e07a06e
realjpower2002 <88989064+realjpower2002@users.noreply.github.com>
2022-11-01 15:24:31 -0500

    making an entire game in 1 commit

--- js/dependencies/2_purgatory.js
+++ js/dependencies/2_purgatory.js
@@ -1,23 +1,80 @@
+//const { rejects } = require("assert");
+
+//const { text } = require("body-parser");
+
+//const { text } = require("body-parser");
+
+//const { text } = require("body-parser");
+
+let numberOfPlayers = 1;
+
 function purgatory() {
   this.setup = function() {
+    this.whoGotFirst; //holds the index of the player who wins the game
+    this.whoGotSecond;
+    this.whoGotThird;
+
+		this.game_length = 30.000;
+		this.start_time;
+		this.current_time = this.game_length;
+
     this.players = [];
     this.main_player_index;
-    this.arrow_keys = [39, 37, 38, 40];
+    this.arrow_keys = [39, 37];
     this.greenSprite = loadImage(repo_address+"media/sprites/Green.png");
     imageMode(CENTER);
-    this.players[0] = new game_1_player(this.greenSprite, 200, 200, 0);
+    this.players[0] = new game_2_player(this.greenSprite, null, this.numberOfPlayers, 3);
     this.main_player_index = 0;
+    this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+    this.current_letter = "g"
+    //data fields for MY game
+		//
+		//stores the current number of players in the game
+		//this.numberOfPlayers = 1;
+		//
   }
 
   this.key_pressed = function(keycode) {
-    for (i=0;i<4;i++){
-      if (keycode == this.arrow_keys[i]){
-        this.players[this.main_player_index].facing = i;
+    if (keycode == this.arrow_keys[0]) { //if the client presses "right"
+      if(this.players[this.main_player_index].previous_key_pressed == this.arrow_keys[1]) {
+        //if client previously pressed the "left" key
+        this.players[this.main_player_index].facing = 0;
         this.players[this.main_player_index].move = 1;
         this.players[this.main_player_index].sx = 0;
         send_data("my_pos:"+this.players[this.main_player_index].make_data_raw());
+        this.players[this.main_player_index].previous_key_pressed = keycode;
+        this.players[this.main_player_index].timePlayerStartedMoving = Date.now();
+        return;
+      } else if(this.players[this.main_player_index].previous_key_pressed == 40) {
+        //if this is the first key the client pressed
+        this.players[this.main_player_index].facing = 0;
+        this.players[this.main_player_index].move = 1;
+        this.players[this.main_player_index].sx = 0;
+        send_data("my_pos:"+this.players[this.main_player_index].make_data_raw());
+        this.players[this.main_player_index].previous_key_pressed = keycode;
+        this.players[this.main_player_index].timePlayerStartedMoving = Date.now();
         return;
       }
+    } else if (keycode == this.arrow_keys[1]) { //if the client presses "left"
+        if(this.players[this.main_player_index].previous_key_pressed == this.arrow_keys[0]) {
+          //if client previously pressed the "right" key
+          this.players[this.main_player_index].facing = 0;
+          this.players[this.main_player_index].move = 1;
+          this.players[this.main_player_index].sx = 0;
+          send_data("my_pos:"+this.players[this.main_player_index].make_data_raw());
+          this.players[this.main_player_index].previous_key_pressed = keycode;
+          this.players[this.main_player_index].timePlayerStartedMoving = Date.now();
+          return;
+        } else if(this.players[this.main_player_index].previous_key_pressed == 40) {
+          //if this is the first key the client pressed
+          this.players[this.main_player_index].facing = 0;
+          this.players[this.main_player_index].move = 1;
+          this.players[this.main_player_index].sx = 0;
+          send_data("my_pos:"+this.players[this.main_player_index].make_data_raw());
+          this.players[this.main_player_index].previous_key_pressed = keycode;
+          this.players[this.main_player_index].timePlayerStartedMoving = Date.now();
+          return;
+        }
     }
   }
 
@@ -36,31 +93,74 @@ function purgatory() {
   this.draw = function() {
     background(200, 200, 200);
     fill(0, 0, 0);
-    text_make(0, 200, 0, 2);
+    text_make(0, 70, 0, 2);
     textAlign(CENTER, CENTER);
-    text("PURGATORY", width/2, height/2);
+    text("RUN!", width/2, height/2);
+    text("Time Remaining: "+this.current_time, width/2,40);
+
+    fill(100,0,0);
+    rect(900,40,50,450);
     for (let i in this.players) {
+      text_make(0, 70, 0, 2);
+
+      this.players[i].y = 400-i * 90;
+
       this.players[i].draw();
+
+      this.playerNum = parseInt(i)+1;
+
+      text(""+this.playerNum,30,this.players[i].y);
+
+      if(this.players[i].x > 900) {
+        if(this.whoGotFirst == null) {
+          this.whoGotFirst = this.playerNum;
+        } 
+        else {
+          if(this.whoGotFirst != this.playerNum && this.whoGotSecond == null) {
+            this.whoGotSecond = this.playerNum;
+          }
+          else {
+            if(this.whoGotFirst!= this.playerNum && this.whoGotSecond != this.playerNum && this.whoGotThird == null) {
+              this.whoGotThird = this.playerNum;
+            }
+          }
+        }
+
+        textAlign(LEFT, CENTER);
+        text_make(0, 20, 0, 2);
+
+        text("Player "+this.whoGotFirst+" Got First!",15,15);
+        if(this.whoGotSecond != null)
+          text("Player "+this.whoGotSecond+" Got Second!",15,35);
+        if(this.whoGotThird != null)
+          text("Player "+this.whoGotThird+" Got Third!",15,55);
+      }
     }
   }
 
   this.read_network_data = function(flag, message) {
     if (flag == "player_count") {
       for (j=this.players.length; j < parseInt(message); j++){
-        this.players[j] = new game_1_player(this.greenSprite, 300, 300, 1);
+        this.players[j] = new game_2_player(this.greenSprite, null, numberOfPlayers, 3);
+        numberOfPlayers++;
+        send_data("debug:"+numberOfPlayers)
       }
     } else if (flag == "assigned_id") {
       this.main_player_index = parseInt(message);
     } else if (flag == "pos_player") {
       this.read_in_player_position(message);
     } else if (flag == "new_player") {
-      this.players[parseInt(message)] = new game_1_player(this.greenSprite, 300, 300, 0);
+      this.players[parseInt(message)] = new game_2_player(this.greenSprite, null, numberOfPlayers, 3);
+      numberOfPlayers++;
     } else if (flag == "rmv_player") {
       var player_index = parseInt(message);
       this.players.splice(player_index, 1);
       if (this.main_player_index > player_index) {
         this.main_player_index -= 1;
       }
+      numberOfPlayers--;
+    } else if (flag == "game_state") {
+      this.read_in_game_state(message);
     }
   }
 
@@ -68,4 +168,10 @@ function purgatory() {
     p_vals = convert_data_string(data_string, [0, 3, 5, 6, 7], [1, 2, 4]);
     this.players[p_vals[0]].update_data(null, p_vals[1], p_vals[2], p_vals[3], p_vals[4], p_vals[5], p_vals[6], p_vals[7]);
   }
+  this.read_in_game_state = function(data_string) {
+		p_vals = convert_data_string(data_string, [0, 1]);
+    this.current_time = Math.max(p_vals[0],0);
+		this.game_length = p_vals[1];
+		this.start_time = millis()/1000;
+	}
 }
--- js/dependencies/2_purgatory_classes.js
+++ js/dependencies/2_purgatory_classes.js
@@ -1,4 +1,4 @@
-class game_1_player {
+class game_2_player {
 	constructor(spriteSheet, x, y, face) {
 		this.spriteSheet = spriteSheet;
 		this.sx = 0;        //Frame counter for when the player is moving.
@@ -68,14 +68,9 @@ class game_1_player {
 	}
 
 	grab_fruit(fruit_id, size){
-		this.fruit_holding = 1;
-		this.fruit_held_id = fruit_id;
-		this.speed = 15/size;
 	}
 
 	drop_fruit(){
-		this.speed = 5;
-		this.fruit_holding = 0;
 	}
 
 	get_pos_string(){
@@ -104,128 +99,4 @@ class game_1_player {
 											this.speed+","+this.facing+","+this.fruit_holding+","+this.fruit_held_id;
 		return string_make;
 	}
-}
-
-class game_1_fruit {
-
-	/*
-	network commands:
-	make_fruit:x,y,size
-	upd_fruit:x,y,size,held,scored,player_holding_id //from server
-
-	*/
-
-	constructor(spriteSheet, x, y, size) {
-		this.spriteSheet = spriteSheet;
-		this.x = x;
-		this.y = y;
-		this.size = int(size);
-		this.held = 0;
-		this.scored = 0;
-		this.player_holding_id = 0;
-		this.sprite_select = 0;
-		if ((size < 5) || (size > 15)) {
-			size = Math.min(15, Math.max(0, 5));
-		}
-
-		if (size > 12) {
-			this.sprite_select = 3;
-		} else if (size > 10) {
-			this.sprite_select = 2;
-		} else if (size > 7) {
-			this.sprite_select = 1;
-		}
-	}
-
-	draw() {
-		if (this.scored) {
-			return;
-		}
-		push();
-		g_cam.translate(this.x, this.y);
-		g_cam.image(this.spriteSheet, null, null, 20, 20, 20*(this.sprite_select), 0, 20, 20);
-		pop();
-	}
-
-	update_position(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	check_grabbed(x, y, player_index) {
-		if (this.held || this.scored) {
-			return;
-		}
-		var player_x_norm = Math.abs(x - this.x),
-				player_y_norm = Math.abs(y - this.y);
-		if ((player_x_norm <= 40) & (player_y_norm <= 40)) {
-			this.held = 1;
-			this.player_holding_id = player_index;
-		}
-	}
-
-	drop() {
-		this.held = 0;
-	}
-
-	update_data(x, y, size, held, scored, player_holding_id) {
-		if (!(isNaN(x)) && x != null) {this.x = x;}
-		if (!(isNaN(y)) && y != null) {this.y = y;}
-		if (!(isNaN(size)) && size != null) {this.size = size;}
-		if (!(isNaN(held)) && held != null) {this.held = held;}
-		if (!(isNaN(scored)) && scored != null) {this.scored = scored;}
-		if (!(isNaN(player_holding_id)) && player_holding_id != null) {this.player_holding_id = player_holding_id;}
-		if (this.size > 12) { this.sprite_select = 3; }
-		else if (this.size > 10) { this.sprite_select = 2; } 
-		else if (this.size > 7) { this.sprite_select = 1; } 
-		else { this.sprite_select = 0; }
-	}
-
-	make_data(index) {
-		var str_make = "pos_fruit:"+str(index)+","+str(this.x)+","+str(this.y)+","+
-										str(this.size)+","+str(this.held)+","+str(this.scored)+","+str(this.player_holding_id);
-		return str_make;
-	}
-}
-
-class game_1_endzone {
-	constructor(x1, x2, y1, y2) {
-		this.x = x1;
-		this.y = y1;
-		this.width = x2 - x1;
-		this.height = y2 - y1;  
-		this.score = 0;
-	}
-
-	draw(){
-		push();
-		text_make(0, 30, 0, 1);
-		fill(255, 204, 0);
-		g_cam.rect(this.x, this.y, this.width, this.height);
-		fill(0, 0, 0);
-		g_cam.text(str(this.score), this.width/2+this.x, this.height/2+this.y);
-		pop();
-	}
-
-	check_placement(x, y){
-		x -= this.x;
-		y -= this.y;
-		if ((x >= 0) && (x <= this.width) && (y >= 0) && (y <= this.height)) {
-			return true;
-		}
-		return false;
-	}
-
-	update_data(x, y, width, height, score){
-		if (!isNaN(x)) { this.x = x; }
-		if (!isNaN(y)) { this.y = y; }
-		if (!isNaN(width)) { this.width = width; }
-		if (!isNaN(height)) { this.height = height; }
-		if (!isNaN(score)) { this.score = score; }
-	}
-
-	make_data(index){
-		return "upd_endzone:"+str(index)+","+str(this.x)+","+str(this.y)+","+str(this.width)+","+str(this.height)+","+str(this.score);
-	}
-
 }
