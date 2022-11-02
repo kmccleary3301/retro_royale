351b56b6ee59c96d05156942718362d331c2ba39
kmccleary3301 <kmccl24@lsu.edu>
2022-11-02 14:39:19 -0500

    Revert "Revert "Merge remote-tracking branch 'Retro_Royale/master' into james_edits""
    This reverts commit be2b2d346d44032cf6284816baa461fe4e15226a.

--- js/dependencies/classes_general.js
+++ js/dependencies/classes_general.js
@@ -45,7 +45,7 @@ class g_camera {
   }
 
   stroke_weight_adjust() {
-    
+    return;
   }
 
   text_size_adjust() {
@@ -61,6 +61,14 @@ class g_camera {
 
   rect(x, y, w, h) {
     rect(this.new_x(x), this.new_y(y), this.new_size(w), this.new_size(h));
+  }
+
+  ellipse(x, y, w, h) {
+    ellipse(this.new_x(x), this.new_y(y), this.new_size(w), this.new_size(h));
+  }
+
+  triangle(x1, y1, x2, y2, x3, y3) {
+    triangle(this.new_x(x1), this.new_y(y1), this.new_x(x2), this.new_y(y2), this.new_x(x3), this.new_y(y3)); 
   }
 }
 
@@ -78,6 +86,7 @@ class button {
   }
 
   draw() {
+    push();
     fill(this.color[0], this.color[1], this.color[2]);
     stroke(10);
     if (this.pressed) {strokeWeight(3);} else {strokeWeight(1);}
@@ -86,7 +95,8 @@ class button {
     textAlign(CENTER, CENTER);
     text_make(0, 0.3*Math.min(this.box_width, this.box_height), 0, 0);
     fill(this.text_color[0], this.text_color[1], this.text_color[2]);
-    text(this.text, this.x_cen, this.y_cen);  
+    text(this.text, this.x_cen, this.y_cen);
+    pop();
   }
 
   check_press(x, y) {
@@ -100,5 +110,102 @@ class button {
 
   activate() {
     this.execute();
+  }
+}
+
+class sprite_animation_object {
+  constructor(sprite, draw_size, tile_width, tile_height, row_dictionary) {
+    //full sprite image, draw size of image, width of each grid tile, height of each grid tile, length of each animation, 
+    this.sprite = sprite;
+    this.sx = 0;
+    this.x_mod = tile_width;
+    this.y_mod = tile_height;
+    this.running = 0;
+    this.draw_size = draw_size;
+    this.w_h_ratio = this.x_mod/this.y_mod;
+    this.row_dictionary = row_dictionary;
+    this.current_animation_row = 0;
+    this.current_row_length = 1;
+    this.flip_image = 0;
+  }
+
+  draw(x, y, use_g_cam) {
+    push();
+    /*
+    all parameters optional.
+    use_g_cam is true or false
+    if x and y not passed, it will draw at 0, 0
+    */
+
+    if (use_g_cam === undefined || !(use_g_cam)) { 
+      var use_g_cam = false; 
+      if (x === undefined) { var x = 0; }
+      if (y === undefined) { var y = 0; }
+    } else {
+      if (x === undefined) { var y = 0; }
+      if (y === undefined) { var y = 0; }
+    }
+    //if (this.flip_image) { console.log("flip1"); scale(-1, 1); console.log("flip2"); }
+    if (this.running) {
+      if (use_g_cam) {
+        g_cam.translate(x+(this.draw_size*this.w_h_ratio)*this.flip_image, y);
+        scale (1-2*this.flip_image, 1);
+        g_cam.image(this.sprite, null, null, this.draw_size*this.w_h_ratio, this.draw_size, 
+                    this.x_mod*this.sx, this.y_mod*this.current_animation_row, this.x_mod, this.y_mod);
+      } else {
+        image(this.sprite, x, y, this.draw_size*this.w_h_ratio, this.draw_size, 
+              this.x_mod*this.sx, this.y_mod*this.current_animation_row, this.x_mod, this.y_mod);
+      }
+      if (frameCount % 6 == 0) {
+        this.sx = (this.sx + 1) % this.current_row_length;
+      }
+    } else {
+      if (use_g_cam) {
+        g_cam.translate(x+(this.draw_size*this.w_h_ratio)*this.flip_image, y);
+        scale (1-2*this.flip_image, 1);
+        g_cam.image(this.sprite, null, null, this.draw_size*this.w_h_ratio, this.draw_size, 
+                    0, this.y_mod*this.current_animation_row, this.x_mod, this.y_mod);
+      } else {
+        image(this.sprite, x, y, this.draw_size*this.w_h_ratio, this.draw_size, 
+              0, this.y_mod*this.current_animation_row, this.x_mod, this.y_mod);
+      }
+    }
+    pop();
+  }
+
+  start() {
+    this.running = 1;
+    this.sx = 0;
+  }
+
+  stop() {
+    this.running = 0;
+    this.sx = 0;
+  }
+
+  change_animation(animation) {
+    if (typeof animation === "string") {
+      console.log("recieved animation as string");
+      this.current_animation_row = this.row_dictionary[animation]["row"];
+      this.current_row_length = this.row_dictionary[animation]["row_length"];
+    } else if (typeof animation === "number") {
+      var key = Object.keys(this.row_dictionary)[animation];
+      this.current_animation_row = this.row_dictionary[key]["row"];
+      this.current_row_length = this.row_dictionary[key]["row_length"];
+    }
+    this.sx = 0;
+  }
+
+  flip(value) {
+    //pass true or false
+    if (value === undefined) {
+      this.flip_image = (this.flip_image+1)%2;
+    } else {
+      if (value) {
+        this.flip_image = 1;
+      } else {
+        this.flip_image = 0;
+      }
+    }
   }
 }
---
+++ server/dependencies/board_game_classes.js
@@ -0,0 +1,118 @@
+class board_game_player { 
+	constructor(x, y, face) {
+		this.x = x;
+		this.y = y;
+		this.move = 0;
+		this.speed = 5;
+		this.facing = face; // use 4, maybe 8 later. 0, 1, 2, 3 for EWNS respectively
+		this.current_tile_index = 0;
+		this.previous_tile_index = 0;
+		this.last_update = Date.now()/1000;
+		this.name = "temp name";
+	}
+
+	update_data(x, y, move, speed, facing, current_tile_index, previous_tile_index, name){
+	  //if (sprite != null) {this.spriteSheet = }
+	  if (x != null) { this.x = x; }
+	  if (y != null) { this.y = y; }
+	  if (move != null) { this.move = move; }
+	  if (speed != null) { this.speed = speed; }
+	  if (facing != null) { this.update_facing(facing); }
+	  if (current_tile_index != null) { this.current_tile_index = current_tile_index; }
+	  if (previous_tile_index != null) { this.previous_tile_index = previous_tile_index; }
+		if (name != null) { this.name = name; }
+	}
+  
+	make_data_raw(){
+	  return this.x+","+this.y+","+this.move+","+this.speed+","+this.facing+","+this.current_tile_index+","+
+						this.previous_tile_index+","+this.name;
+	}
+	
+	make_data(player_index){
+	  return "pos_player:"+player_index+","+this.make_data_raw();
+	}
+}
+
+class board_game_tile {
+	constructor(tile_x, tile_y, tile_type, tile_access_directions) {
+		this.tile_x = tile_x;
+		this.tile_y = tile_y;
+		this.x = 80 + 160*this.tile_x;
+		this.y = 80 + 160*this.tile_y;
+		this.type = tile_type;
+		this.directions = tile_access_directions;
+		this.connected_tiles = {
+			"right" : {
+				"connected" : 0,
+				"is_child": 0,
+				"is_parent": 0,
+				"tile_id": 0
+			},
+			"left": {
+				"connected" : 0,
+				"is_child": 0,
+				"is_parent": 0,
+				"tile_id": 0
+			},
+			"up": {
+				"connected" : 0,
+				"is_child": 0,
+				"is_parent": 0,
+				"tile_id": 0
+			},
+			"down": {
+				"connected" : 0,
+				"is_child": 0,
+				"is_parent": 0,
+				"tile_id": 0
+			},
+		};
+	}
+
+	make_child(direction, id) {
+		this.connected_tiles[direction]["connected"] = 1;
+		this.connected_tiles[direction]["is_child"] = 1;
+		this.connected_tiles[direction]["tile_id"] = id;
+	}
+
+	make_parent(direction, id) {
+		this.connected_tiles[direction]["connected"] = 1;
+		this.connected_tiles[direction]["is_parent"] = 1;
+		this.connected_tiles[direction]["tile_id"] = id;
+	}
+
+	check_child (direction) {
+		return (this.connected_tiles[direction]["connected"] && this.connected_tiles[direction]["is_child"]); 
+	}
+
+	update_data(tile_x, tile_y, x, y, type, r1, r2, r3, 
+						r4, l1, l2, l3, l4, u1, u2, u3, u4, d1, d2, d3, d4) {
+		if (tile_x != null) { this.tile_x = tile_x; }
+		if (tile_y != null) { this.tile_y = tile_y; }
+		if (x != null) { this.x = x; }
+		if (y != null) { this.y = y; }
+		if (type != null) {this.type = type; }
+		var connected_tiles_array = [[r1, r2, r3, r4], [l1, l2, l3, l4], [u1, u2, u3, u4], [d1, d2, d3, d4]];
+		for (let i in Object.keys(this.connected_tiles)) {
+			var key_1 = Object.keys(this.connected_tiles)[i];
+			for (let j in Object.keys(this.connected_tiles[key_1])) {
+				var key_2 = Object.keys(this.connected_tiles[key_1])[j];
+				if (connected_tiles_array[i][j] != null) { this.connected_tiles[key_1][key_2] = connected_tiles_array[i][j]; }
+			}
+		}
+	}
+
+	make_data_raw() {
+		str_make = this.tile_x+","+this.tile_y+","+this.x+","+this.y+","+this.type;
+		for (let i in this.connected_tiles) {
+			for (let j in this.connected_tiles[i]) { str_make += ","+this.connected_tiles[i][j]; }
+		}
+		return str_make;
+	}
+
+	make_data(tile_id){
+		return "tile_pos:"+tile_id+","+this.make_data_raw();
+	}
+}
+
+module.exports = {board_game_player, board_game_tile};
