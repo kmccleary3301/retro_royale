03bb012a7e3e8ca1532635a18c2014957349565a
Lydia Parsa <lparsa1@lsu.edu>
2022-11-08 15:16:46 -0600

    Revert "enjolie commit"
    This reverts commit 1c7a7c5f114e5300abadd8f17ec3b1f8168dcc42.

--- js/dependencies/ball_game.js
+++ js/dependencies/ball_game.js
@@ -15,55 +15,8 @@ class game_2_ball {
       this.speed = 300;
       this.last_update = millis()/1000;
     }
-  }
-    this.setup = function() {
-      pop();
-      test_reset_draw_settings();
-      //reset();
-      this.camera_scale = 1;
-      this.players = [];
-      this.greenSprite = loadImage(repo_address+"media/sprites/Green.png");
-      this.green_sprite_2 = loadImage("media/sprites/spelunky_simple.png");
-      this.tiles = [];
-      this.event_timer_start = millis()/1000;
-      this.event_timer = 0;
-      this.center_on_player = true;
-      this.starting_camera_coordinates = [0, 0];
-      this.camera_center_coordinates = [0, 0];
-      this.mouse_held = false;
-      this.mouse_click_location = [0, 0];
-      //this.arrow_keys = [39, 37, 38, 40];
-      this.arrow_keys = {
-        "left" : 37,
-        "right" : 39,
-        "up" : 38,
-        "down" : 40
-      };
-      this.tile_grid_dimensions = [50, 50];
-      this.make_board_layout_preset_1();
-      this.players[0] = new board_game_player(this.green_sprite_2, 0, 0, 3);
-      this.players[0].x = this.tiles[0].x;
-      this.players[0].y = this.tiles[0].y;
-      this.user_player_index = 0;		//Player controlled by the client
-      this.turning_player_index = 0; 	//Player currently rolling dice
-      this.next_turning_player_index = 0;
-      this.animation_info = [0, 0, 0, 0, 0, 0]; //boolean, player_id, tile_start, tile_end, start_time, direction.
-      this.animation_queue = [];
-      this.current_turn = 1;
-      this.current_turn_moves = 0;
-  
-      this.buttons = {
-        "overlay" : []
-      };
-      this.current_button_menu = "overlay";
-      this.buttons["overlay"][0] = new button(50, 50, 50, 50, [255, 78, 0], [10, 10, 10], "Center");
-      this.buttons["overlay"][1] = new button(50, 125, 50, 50, [255, 78, 0], [10, 10, 10], "Menu");
-  
-      //image_process("media/board_templates/test_template_1.png", parse_board_from_image);
-      this.make_board_layout_preset_1();
-    
-  
-    draw();{
+
+    draw() {
       //console.log("drawing ball");
       fill(30, 0, 20);
       stroke(255);
@@ -93,19 +46,16 @@ class game_2_ball {
       this.dx /= factor;
       this.last_update = millis()/1000;
     }
-  }
-  
 
-    update_data(x, y, dx, dy, speed); {
+    update_data(x, y, dx, dy, speed) {
       this.x = x;
       this.y = y;
       this.dx = dx;
       this.dy = dy;
       this.speed = speed;
     }
-  
 
-
+}
 
 function ball_game() {
   this.setup = function() {
@@ -157,47 +107,7 @@ function ball_game() {
     
     
   }
-  this.animate_walking = function() {
-		this.players[this.animation_info[1]].draw();
-		var check_end = false;
-		if (this.animation_info[5] == "right") {
-			if (this.players[this.animation_info[1]].x >= this.tiles[this.animation_info[3]].x) {
-				check_end = true;
-			}
-		} else if (this.animation_info[5] == "left") {
-			if (this.players[this.animation_info[1]].x <= this.tiles[this.animation_info[3]].x) {
-				check_end = true;
-			}
-		} else if (this.animation_info[5] == "up") {
-			if (this.players[this.animation_info[1]].y <= this.tiles[this.animation_info[3]].y) {
-				check_end = true;
-			}
-		} else if (this.animation_info[5] == "down") {
-			if (this.players[this.animation_info[1]].y >= this.tiles[this.animation_info[3]].y) {
-				check_end = true;
-			}
-		}
-		if (check_end) {
-			console.log("check_end is true");
-			this.players[this.animation_info[1]].y = this.tiles[this.animation_info[3]].y;
-			this.players[this.animation_info[1]].x = this.tiles[this.animation_info[3]].x;
-			this.players[this.animation_info[1]].update_moving(false);
-			this.players[this.animation_info[1]].previous_tile_index = this.players[this.animation_info[1]].current_tile_index;
-			this.players[this.animation_info[1]].current_tile_index = this.animation_info[3];
-			this.animation_info[0] = 0;
-			this.reset_event_timer();
-			if (this.current_turn_moves <= 0) {
-				this.tile_event_trigger(this.tiles[this.animation_info[3]].type);
-			} else if (this.turning_player_index == this.user_player_index) {
-				var walkable_directions = this.check_walkable_directions(this.user_player_index);
-				console.log("Checking walkable directions: "+walkable_directions);
-				if (walkable_directions.length == 1) {
-					send_data("move_tile_direction:"+walkable_directions[0]);
-				}
-			}
-			
-		}
-	}
+
   this.read_network_data = function(flag, message) {
     if (flag == "player_count") {
       for (j=this.players.length; j < parseInt(message); j++){
@@ -221,21 +131,6 @@ function ball_game() {
       seed_get = parseInt(message);
     }
   }
-  this.key_pressed = function(keycode) {
-		console.log("KEY PRESSED: "+keycode);
-		console.log("User_player_index: "+this.user_player_index);
-		if (this.user_player_index != this.turning_player_index || this.animation_queue.length > 0) { return; }
-		if (this.current_turn_moves <= 0) { return; }
-		for (let i in this.arrow_keys){
-			if (keycode == this.arrow_keys[i]){
-				send_data("move_tile_direction:"+i);
-			}
-		}
-	}
-
-	this.key_released = function(keycode) {
-		return;
-	}
 
   this.read_in_player_position = function(data_string) { //format packet as pos_player:id,x,y,move,speed,facing,fruit_holding,fruit_id
     p_vals = convert_data_string(data_string, [0, 3, 5, 6, 7], [1, 2, 4]);
