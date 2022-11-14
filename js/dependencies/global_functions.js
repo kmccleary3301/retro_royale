6e4d659caf9cf68190bef902a77244280544c329
kmccleary3301 <kmccl24@lsu.edu>
2022-11-14 11:05:04 -0600

    test function added, please ignore

--- js/dependencies/global_functions.js
+++ js/dependencies/global_functions.js
@@ -83,3 +83,64 @@ function test_reset_draw_settings() {
   scale(1, 1);
   textSize(10);
 }
+
+function stringf(str_in) {
+  // |n10l# or ..|d10r# for ints, 10 spaces align to left or right
+  // |f10.4l# 10 spaces, 4 decimals precise, align to left
+  // |s10l# 10 spaces, align to left.
+
+  var params = [];
+  for (i = 1; i < arguments.length; i++) { params[params.length] = arguments[i]; }
+
+  var marker_array = [], new_string = str_in;
+  var temp_ind = 0, value_string = "", values, marker_var, temp_rep_string, arg_string, t1, t2;
+  for (let i in str_in) {
+    if (i > str_in.length-3) { break; }
+    if (str_in[i] == '|') {
+      if ("nfs".includes(str_in[int(i)+1])) {
+        temp_ind = int(i)+2;
+        while ('0123456789.'.includes(str_in[temp_ind])) {
+          temp_ind++;
+        }
+        if ('lr'.includes(str_in[temp_ind]) && str_in[temp_ind+1] == '#') {
+          marker_var = {
+            "type" : "empty",
+            "frmt_size": 0,
+            "precision": 0,
+            "alignment": "left",
+            "index_in_input_string": int(i),
+            "length_of_marker": temp_ind - int(i) + 2
+          }
+          if (str_in[int(i)+1] == 'n') { marker_var["type"] = "int"; }
+          else if (str_in[int(i)+1] == 'f') { marker_var["type"] = "float"; }
+          else if (str_in[int(i)+1] == 's') { marker_var["type"] = "string"; }
+          if (str_in[temp_ind] == 'l') { marker_var["alignment"] = "left"; }
+          else if (str_in[temp_ind] == 'r') { marker_var["alignment"] = "right"; }
+          value_string = str_in.substring(int(i)+2, temp_ind);
+          var values = value_string.split(".");
+          marker_var["frmt_size"] = parseInt(values[0]);
+          if (marker_var["type"] == "float") { marker_var["precision"] = parseInt(values[1]); }
+          marker_array[marker_array.length] = marker_var;
+        }
+      }
+    }
+  }
+  if (params.length < marker_array.length) { marker_array.splice(params.length, marker_array.length-params.length); }
+  for (var i = marker_array.length-1; i >= 0; i--) {
+    arg_string = str(params[i]);
+    if (marker_array[i]["type"] == "float") {
+      var dot_index = arg_string.indexOf(".");
+      if (arg_string.length > dot_index + marker_array[i]["precision"]) {
+        arg_string = arg_string.substring(0, dot_index+marker_array[i]["precision"]+1);
+      }
+    }
+    temp_rep_string = "";
+    for (j=arg_string.length; j <= marker_array[i]["frmt_size"]; j++) { temp_rep_string += " "; }
+    if (marker_array[i]["alignment"] == "left") { temp_rep_string = arg_string + temp_rep_string; }
+    else if (marker_array[i]["alignment"] == "right") { temp_rep_string = temp_rep_string + arg_string; }
+    t1 = marker_array[i]["index_in_input_string"];
+    t2 = t1 + marker_array[i]["length_of_marker"];
+    new_string = new_string.substring(0, t1) + temp_rep_string + new_string.substring(t2, new_string.length);
+  }
+  return new_string;
+}
